916604cdaa572267b0301638940cd49b42b89ada
joaobestett <bestettijoao@gmail.com>
2025-10-15 22:35:09 -0300

    "fix/troca do any

--- lib/api.tsx
+++ lib/api.tsx
@@ -1,10 +1,18 @@
-import axios from "axios";
+// lib/api.ts
+import axios, { AxiosHeaders, InternalAxiosRequestConfig } from "axios";
 import { storage } from "./storage";
 
-export const api = axios.create({ baseURL: "http://localhost:3000" });
+export const api = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_URL || "http://127.0.0.1:3000",
+});
 
-api.interceptors.request.use(async (cfg) => {
+api.interceptors.request.use(async (cfg: InternalAxiosRequestConfig) => {
   const token = await storage.getItem("access_token");
-  if (token) cfg.headers = { ...(cfg.headers || {}), Authorization: `Bearer ${token}` };
+  if (token) {
+    // garante que é AxiosHeaders e usa .set()
+    const headers = AxiosHeaders.from(cfg.headers);
+    headers.set("Authorization", `Bearer ${token}`);
+    cfg.headers = headers;
+  }
   return cfg;
 });
